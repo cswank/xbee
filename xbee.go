5cd4b508c8bb1c3450bd9d6cced8ab3a2c708378
Craig Swank <craigswank@gmail.com>
2016-10-19 23:28:17 +0000

    README update and code refactor

--- xbee.go
+++ xbee.go
@@ -29,7 +29,8 @@ var (
 
 //NewMessage parses a xbee messsage (from the 3rd byte on).
 //The first 3 bytes are used to detect the beginning of a new
-//message and the length of the message.
+//message and the length of the message so are only useful for
+//ingesting data from an xbee.
 func NewMessage(data []byte) (Message, error) {
 	var h header
 	buf := bytes.NewReader(data)
@@ -52,12 +53,31 @@ func (x *Message) payload() []byte {
 	return x.frame[16 : len(x.frame)-1]
 }
 
+//GetAnalog returns a map of the adc pins and their voltages (in mV).
+//If no pins are configured for adc then it returns an empty map.
 func (x *Message) GetAnalog() (map[string]float64, error) {
 	m := map[string]float64{}
 	if x.AnalogChanMask == 0 {
 		return m, nil
 	}
 
+	vals, err := x.getRawAnalog()
+	if err != nil {
+		return m, err
+	}
+
+	var j int
+	for i, o := range adc {
+		if uint8(1<<uint8(i))&x.AnalogChanMask > 0 {
+			x := vals[j]
+			m[o] = 1200.0 * float64(x) / float64(1023)
+			j++
+		}
+	}
+	return m, nil
+}
+
+func (x *Message) getRawAnalog() ([]uint16, error) {
 	var d []byte
 	payload := x.payload()
 	if x.DigitalChanMask == 0 {
@@ -68,20 +88,7 @@ func (x *Message) GetAnalog() (map[string]float64, error) {
 
 	f := make([]uint16, len(d)/2)
 	buf := bytes.NewReader(d)
-	err := binary.Read(buf, binary.BigEndian, &f)
-	if err != nil {
-		return nil, err
-	}
-
-	var j int
-	for i, o := range adc {
-		if uint8(1<<uint8(i))&x.AnalogChanMask > 0 {
-			x := f[j]
-			m[o] = 1200.0 * float64(x) / float64(1023)
-			j++
-		}
-	}
-	return m, nil
+	return f, binary.Read(buf, binary.BigEndian, &f)
 }
 
 func (x *Message) GetDigital() (map[string]bool, error) {
@@ -90,13 +97,9 @@ func (x *Message) GetDigital() (map[string]bool, error) {
 		return m, nil
 	}
 
-	payload := x.payload()[:3]
-
-	var val uint16
-	buf := bytes.NewReader(payload)
-	err := binary.Read(buf, binary.BigEndian, &val)
+	val, err := x.getRawDigital()
 	if err != nil {
-		return nil, err
+		return m, err
 	}
 
 	var j int
@@ -108,6 +111,12 @@ func (x *Message) GetDigital() (map[string]bool, error) {
 		}
 	}
 	return m, nil
+}
+func (x *Message) getRawDigital() (uint16, error) {
+	payload := x.payload()[:3]
+	var val uint16
+	buf := bytes.NewReader(payload)
+	return val, binary.Read(buf, binary.BigEndian, &val)
 }
 
 func (x *Message) Check() bool {
