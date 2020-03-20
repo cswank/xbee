66519979701570b0fd8b9e776b5da272aadec946
Craig Swank <craigswank@gmail.com>
2020-03-20 06:38:04 -0600

    go modules, get rid of ginkgo

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/cswank/xbee
+
+go 1.14
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/creack/goselect v0.1.1 // indirect
+	github.com/stretchr/testify v1.4.0
+	go.bug.st/serial.v1 v0.0.0-20191202182710-24a6610f0541
+	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
--- xbee_test.go
+++ xbee_test.go
@@ -1,46 +1,26 @@
 package xbee_test
 
 import (
-	"github.com/cswank/xbee"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-
 	"testing"
+
+	"github.com/cswank/xbee"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestXbee(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Xbee Suite")
+	d := []byte{0x92, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x4C, 0x0E, 0xBE, 0x61, 0x59, 0x01, 0x01, 0x00, 0x18, 0x03, 0x00, 0x10, 0x02, 0x2F, 0x01, 0xFE, 0x49}
+	x, err := xbee.NewMessage(d)
+	assert.NoError(t, err)
+
+	adc, err := x.GetAnalog()
+	assert.NoError(t, err)
+	assert.InDelta(t, 655.7184, adc["adc0"], 0.01)
+	assert.InDelta(t, 598.2405, adc["adc1"], 0.01)
+
+	dio, err := x.GetDigital()
+	assert.NoError(t, err)
+	v, ok := dio["dio3"]
+	assert.True(t, ok)
+	assert.False(t, v)
+	assert.True(t, dio["dio4"])
 }
-
-var _ = Describe("xbee message", func() {
-
-	var (
-		x xbee.Message
-	)
-
-	BeforeEach(func() {
-		var err error
-		d := []byte{0x92, 0x00, 0x13, 0xA2, 0x00, 0x40, 0x4C, 0x0E, 0xBE, 0x61, 0x59, 0x01, 0x01, 0x00, 0x18, 0x03, 0x00, 0x10, 0x02, 0x2F, 0x01, 0xFE, 0x49}
-		x, err = xbee.NewMessage(d)
-		Expect(err).To(BeNil())
-	})
-
-	It("gets the analog values", func() {
-		vals, err := x.GetAnalog()
-		Expect(err).To(BeNil())
-		Expect(vals).To(HaveLen(2))
-		Expect(vals["adc0"]).To(BeNumerically("~", 655.7, 0.1))
-		Expect(vals["adc1"]).To(BeNumerically("~", 598.2, 0.1))
-	})
-
-	It("gets the digital values", func() {
-		vals, err := x.GetDigital()
-		Expect(err).To(BeNil())
-		Expect(vals).To(HaveLen(2))
-		v, ok := vals["dio3"]
-		Expect(ok).To(BeTrue())
-		Expect(v).To(BeFalse())
-		Expect(vals["dio4"]).To(BeTrue())
-	})
-})
